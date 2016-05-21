b0f7b5a6085f19122dafd203e07b2a00751d0368
dkellam9 <dkellam9@gmail.com>
2016-05-21 17:28:21 +0000

    wiped advertisements

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504220120) do
-
-  create_table "advertisments", force: :cascade do |t|
-    t.string   "title"
-    t.text     "copy"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20160427200855) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
