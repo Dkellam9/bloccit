c00249556682b515ab6bfb21e641e61d1c2762e8
dkellam9 <dkellam9@gmail.com>
2016-07-06 23:47:04 +0000

    Assignment 38 v1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
  
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-
     if user && user.authenticate(params[:session][:password])
       create_session(user)
       flash[:notice] = "Welcome, #{user.name}!"
