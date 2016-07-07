260a03ec7874789143d088fd155a3ae1a4e8ca51
dkellam9 <dkellam9@gmail.com>
2016-07-07 00:57:23 +0000

    Checkpoint 39 Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :require_sign_in, except: :show
+  
   def show
     @post = Post.find(params[:id])
   end
@@ -9,11 +11,10 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
     @topic = Topic.find(params[:topic_id])
-    @post.topic = @topic
+    @post = @topic.posts.build(post_params)
+    @post.user = current_user
+    
     if @post.save
       flash[:notice] = "Post was saved successfully."
       redirect_to [@topic, @post]
@@ -29,19 +30,20 @@ class PostsController < ApplicationController
   
   def update
     @post = Post.find(params[:id])
-     @post.title = params[:post][:title]
-     @post.body = params[:post][:body]
-     if @post.save
-       flash[:notice] = "Post was updated successfully."
-       redirect_to [@post.topic, @post]
-     else
-       flash.now[:alert] = "There was an error saving the post. Please try again."
-       render :edit
-     end
+    @post.assign_attributes(post_params)
+     
+    if @post.save
+      flash[:notice] = "Post was updated successfully."
+      redirect_to [@post.topic, @post]
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
   end
   
   def destroy
      @post = Post.find(params[:id])
+     
      if @post.destroy
        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
        redirect_to @post.topic
@@ -49,5 +51,10 @@ class PostsController < ApplicationController
        flash.now[:alert] = "There was an error deleting the post."
        render :show
      end
+  end
+  private
+ 
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+    )
+end
+users = User.all
+
 # create topics
 15.times do
   Topic.create!(
@@ -19,6 +29,7 @@ topics = Topic.all
 # create posts
 50.times do
   Post.create!(
+    user:   users.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
     )
@@ -33,7 +44,14 @@ posts = Post.all
     ) 
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'dkellam9@gmail.com',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
