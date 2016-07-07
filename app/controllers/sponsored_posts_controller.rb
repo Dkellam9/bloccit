fb6889866e77ef284b62e21bb1fe07e08add5b0e
dkellam9 <dkellam9@gmail.com>
2016-07-07 18:12:48 +0000

    Assignment 34 Incomplete v2

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -6,6 +6,21 @@ class SponsoredPostsController < ApplicationController
   def new
     @sponsoredpost = SponsoredPost.new
   end
+  
+  def create
+     @sponsoredpost = Post.new
+     @sponsoredpost.title = params[:sponsoredpost][:title]
+     @sponsoredpost.body = params[:sponsoredpost][:body]
+     @sponsoredpost.price = params[:sponsoredpost][:price]
+
+     if @sponsoredpost.save
+       flash[:notice] = "Sponsored post was saved successfully."
+       redirect_to @sponsoredpost
+     else
+       flash.now[:alert] = "There was an error saving the sponsored post. Please try again."
+       render :new
+     end
+  end
 
   def edit
     @sponsoredpost = Post.find(params[:id])
