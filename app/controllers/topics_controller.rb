fdd56a5a2c287b36289e5e0e312b6b1ffae85f39
dkellam9 <dkellam9@gmail.com>
2016-07-15 23:53:57 +0000

    Assignment 40 v1

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,7 @@
 class TopicsController < ApplicationController
   before_action :require_sign_in, except: [:index, :show]
   before_action :authorize_user, except: [:index, :show]
+  before_action :authorize_mod, except: 
   
   def index
     @topics = Topic.all
@@ -66,6 +67,13 @@ class TopicsController < ApplicationController
   def authorize_user
      unless current_user.admin?
        flash[:alert] = "You must be an admin to do that."
+       redirect_to topics_path
+     end
+  end
+  
+  def authorize_mod
+     unless current_user.moderator?
+       flash[:alert] = "You must be an moderator to do that."
        redirect_to topics_path
      end
   end
