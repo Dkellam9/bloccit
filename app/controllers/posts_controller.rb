fff626f47720107acc518f57e60f07e479030e17
dkellam9 <dkellam9@gmail.com>
2016-08-02 19:42:52 +0000

    Assignment 40 Completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
-  before_action :authorize_user, except: [:show, :new, :create]
+  before_action :authorize_admin, except: [:show, :new, :create]
+  before_action :authorize_mod, except [:show, :new, :create, :destroy]
   
   def show
     @post = Post.find(params[:id])
@@ -60,12 +61,19 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :body)
   end
   
-  def authorize_user
+  def authorize_admin
      post = Post.find(params[:id])
      unless current_user == post.user || current_user.admin?
        flash[:alert] = "You must be an admin to do that."
        redirect_to [post.topic, post]
      end
   end
-
+  
+  def authorize_mod
+    post = Post.find(params[:id])
+    unless current_user == post.user || current_user.admin? || current_user.moderator?
+      flash[:alert] = "You must be an admin or moderator to do that."
+      redirect_to [post.topic, post]
+    end
+  end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,7 @@
 class TopicsController < ApplicationController
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, except: [:index, :show]
-  before_action :authorize_mod, except: 
+  before_action :authorize_admin, except: [:index, :show, :update]
+  before_action :authorize_mod, except: [:index, :show, :new, :create, :edit, :destroy]
   
   def index
     @topics = Topic.all
@@ -64,7 +64,7 @@ class TopicsController < ApplicationController
     params.require(:topic).permit(:name, :description, :public)
   end
   
-  def authorize_user
+  def authorize_admin
      unless current_user.admin?
        flash[:alert] = "You must be an admin to do that."
        redirect_to topics_path
@@ -72,9 +72,9 @@ class TopicsController < ApplicationController
   end
   
   def authorize_mod
-     unless current_user.moderator?
-       flash[:alert] = "You must be an moderator to do that."
-       redirect_to topics_path
-     end
+    unless current_user.admin? || current_user.moderator?
+      flash[:alert] = "You must be an admin or moderator to do that."
+      redirect_to topics_path
+    end
   end
 end
