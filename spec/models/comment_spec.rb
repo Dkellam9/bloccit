6896e96acf20dae292ae8d5c63e6f3c8bf8312c3
dkellam9 <dkellam9@gmail.com>
2016-08-04 19:59:01 +0000

    Assignment 42 v1

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Comment, type: :model do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to validate_presence_of(:body) }
     it { is_expected.to validate_length_of(:body).is_at_least(5) }
+    it {is_expected.to belong_to(:topic) }
   
   describe "attributes" do
     it "has a body attribute" do
