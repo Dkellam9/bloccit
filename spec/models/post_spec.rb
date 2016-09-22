c951ea0b52aa442459b5188ff99d3e4b56863b03
dkellam9 <dkellam9@gmail.com>
2016-09-22 00:55:32 +0000

    Checkpoint 44 Completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe Post, type: :model do
   
   it { is_expected.to have_many(:comments) }
   it { is_expected.to have_many(:votes) }
+  it { is_expected.to have_many(:favorites) }
   it { is_expected.to belong_to(:topic) }
   it { is_expected.to belong_to(:user) }
   
@@ -73,6 +74,22 @@ RSpec.describe Post, type: :model do
         old_rank = post.rank
         post.votes.create!(value: -1)
         expect(post.rank).to eq (old_rank - 1)
+      end
+    end
+    
+    describe "#create_vote" do
+      it "sets the post upvotes to 1" do
+        expect(post.up_votes).to eq(1)
+      end
+      
+      it "calls #create_vote when post is created" do
+        post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+        expect(post).to receive(:create_vote)
+        post.save
+      end
+      
+      it "associates vote with owner" do
+        expect(post.votes.first.user).to eq(post.user)
       end
     end
   end
